7c92b4386b815f336faebb4746fae01699dc3fbd
Brad Geesaman <bradgeesaman+github@gmail.com>
2020-03-13 11:20:42 -0400

    fields

--- controls/k8s.rb
+++ controls/k8s.rb
@@ -19,17 +19,26 @@ kversion = k8sversion()
 control "k8s-1" do
   impact 1.0
   title "Ensure pods only run in desired namespaces"
-  desc ""
+  desc "description"
+  desc "remediation", "remediation"
 
-  describe "default namespace pods" do
+  tag domain: "01. Identity and Access Management"
+  tag platform: "K8S"
+  tag category: "Identity and Access Management"
+  tag resource: "Pods"
+  tag effort: 0.2
+
+  ref "ref1", url: "https://ref1.local"
+
+  describe "default namespace: pods" do
     subject { k8sobjects(api: 'v1', type: 'pods', namespace: 'default').items }
     its('length') { should cmp 0 }
   end
-  describe "kube-public namespace pods" do
+  describe "kube-public namespace: pods" do
     subject { k8sobjects(api: 'v1', type: 'pods', namespace: 'kube-public').items }
     its('length') { should cmp 0 }
   end
-  describe "kube-node-lease namespace pods" do
+  describe "kube-node-lease namespace: pods" do
     subject { k8sobjects(api: 'v1', type: 'pods', namespace: 'kube-node-lease').items }
     its('length') { should cmp 0 }
   end
@@ -39,8 +48,17 @@ control "k8s-2" do
   impact 1.0
   title "Ensure Dashboard is not running"
   desc "Ensure Dashboard is not running"
+  desc "remediation", "remediation"
 
-  describe "Dashboard pods" do
+  tag domain: "01. Identity and Access Management"
+  tag platform: "K8S"
+  tag category: "Identity and Access Management"
+  tag resource: "Pods"
+  tag effort: 0.2
+
+  ref "ref1", url: "https://ref1.local"
+
+  describe "Dashboard: pods" do
     subject { k8sobjects(api: 'v1', type: 'pods', labelSelector: 'k8s-app=kubernetes-dashboard').items }
     its('length') { should cmp 0 }
   end
@@ -50,9 +68,18 @@ control "k8s-3" do
   impact 1.0
   title "Ensure Tiller is not running"
   desc "Ensure Tiller is not running"
+  desc "remediation", "remediation"
+
+  tag domain: "01. Identity and Access Management"
+  tag platform: "K8S"
+  tag category: "Identity and Access Management"
+  tag resource: "Pods"
+  tag effort: 0.2
+
+  ref "ref1", url: "https://ref1.local"
 
   k8sobjects(api: 'v1', type: 'namespaces').items.each do |ns|
-    describe "#{ns.name}/tiller-deploy deployment" do
+    describe "#{ns.name}/tiller-deploy: deployment" do
       subject { k8sobject(api: 'extensions/v1beta1', type: 'deployments', namespace: ns.name, name: "tiller-deploy") }
       it { should_not exist }
     end
@@ -63,9 +90,18 @@ control "k8s-4" do
   impact 1.0
   title "No pods with latest container tag"
   desc ""
+  desc "remediation", "remediation"
+
+  tag domain: "01. Identity and Access Management"
+  tag platform: "K8S"
+  tag category: "Identity and Access Management"
+  tag resource: "Pods"
+  tag effort: 0.2
+
+  ref "ref1", url: "https://ref1.local"
 
   k8sobjects(api: 'v1', type: 'pods').items.each do |pod|
-    describe "#{pod.namespace}/#{pod.name} pod" do
+    describe "#{pod.namespace}/#{pod.name}: pod" do
       subject { k8sobject(api: 'v1', type: 'pods', namespace: pod.namespace, name: pod.name) }
       it { should_not have_latest_container_tag }
     end
@@ -76,15 +112,24 @@ control "k8s-5" do
   impact 1.0
   title "Validate NetworkPolicy enforcement is installed"
   desc "Ensure Calico is installed on EKS/GKE or Azure CNI is installed on AKS"
+  desc "remediation", "remediation"
+
+  tag domain: "01. Identity and Access Management"
+  tag platform: "K8S"
+  tag category: "Identity and Access Management"
+  tag resource: "Pods"
+  tag effort: 0.2
+
+  ref "ref1", url: "https://ref1.local"
 
   calico = k8sobjects(api: 'apps/v1', type: 'daemonsets', namespace: 'kube-system', labelSelector: 'k8s-app=calico-node').items
   azure = k8sobjects(api: 'apps/v1', type: 'daemonsets', namespace: 'kube-system', labelSelector: 'component=azure-cni-networkmonitor').items
   describe.one do
-    describe "NetworkPolicy Daemonset installed" do
+    describe "NetworkPolicy Daemonset: installed" do
       subject { calico }
       its('count') { should be > 0 }
     end
-    describe "NetworkPolicy Daemonset installed" do
+    describe "NetworkPolicy Daemonset: installed" do
       subject { azure }
       its('count') { should be > 0 }
     end
@@ -95,15 +140,24 @@ control "k8s-6" do
   impact 1.0
   title "Validate PodSpec enforcement is installed"
   desc "Ensure OPA/Gatekeeper, K-rail, or PSP is installed"
+  desc "remediation", "remediation"
+
+  tag domain: "01. Identity and Access Management"
+  tag platform: "K8S"
+  tag category: "Identity and Access Management"
+  tag resource: "Pods"
+  tag effort: 0.2
+
+  ref "ref1", url: "https://ref1.local"
 
   gatekeeper = k8sobjects(api: 'extensions/v1beta1', type: 'deployments', labelSelector: 'control-plane=controller-manager').items
   krail = k8sobjects(api: 'extensions/v1beta1', type: 'deployments', labelSelector: 'name=k-rail').items
   describe.one do
-    describe "OPA/Gatekeeper installed" do
+    describe "OPA/Gatekeeper: installed" do
       subject { gatekeeper }
       its('count') { should be > 0 }
     end
-    describe "K-rail installed" do
+    describe "K-rail: installed" do
       subject { krail }
       its('count') { should be > 0 }
     end
@@ -114,10 +168,19 @@ end
 #  impact 1.0
 #  title "Validate ResourceQuotas"
 #  desc ""
+#  desc "remediation", "remediation"
+#
+#  tag domain: "01. Identity and Access Management"
+#  tag platform: "K8S"
+#  tag category: "Identity and Access Management"
+#  tag resource: "Pods"
+#  tag effort: 0.2
+#
+#  ref "ref1", url: "https://ref1.local"
 #
 #  k8sobjects(api: 'v1', type: 'namespaces').items.each do |ns|
 #    next if ns.name == "kube-node-lease" || ns.name == "kube-public"
-#    describe "#{ns.name} namespace resourcequotas" do
+#    describe "#{ns.name}: namespace resourcequotas" do
 #      subject { k8sobjects(api: 'v1', type: 'resourcequotas', namespace: ns.name).items }
 #      its('count') { should be > 0 }
 #    end
@@ -134,10 +197,19 @@ end
 #  impact 1.0
 #  title "Validate LimitRange"
 #  desc ""
+#  desc "remediation", "remediation"
+#
+#  tag domain: "01. Identity and Access Management"
+#  tag platform: "K8S"
+#  tag category: "Identity and Access Management"
+#  tag resource: "Pods"
+#  tag effort: 0.2
+#
+#  ref "ref1", url: "https://ref1.local"
 #
 #  k8sobjects(api: 'v1', type: 'namespaces').items.each do |ns|
 #    next if ns.name == "kube-node-lease" || ns.name == "kube-public"
-#    describe "#{ns.name} namespace limitranges" do
+#    describe "#{ns.name}: namespace limitranges" do
 #      subject { k8sobjects(api: 'v1', type: 'limitranges', namespace: ns.name).items }
 #      its('count') { should be > 0 }
 #    end
